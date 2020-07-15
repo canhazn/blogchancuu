9c0a9cc0617501010ee1272f85523abd32f3ebe8
tuan anh <canhazn@gmail.com>
2020-07-15 15:10:58 +0700

    Set Paging

--- blogchancuu/settings.py
+++ blogchancuu/settings.py
@@ -177,5 +177,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # '''
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 8
 }
