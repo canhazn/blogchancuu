6b1ba609c13f9f1455b11268ee621bafe6d41dc2
tuan anh <canhazn@gmail.com>
2020-09-01 06:56:16 +0700

    Fix lost database

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Tag(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
     slug = models.SlugField(max_length=200, null=False, blank=False)
-    created_on = models.DateTimeField(auto_now_add=True)
+    # created_on = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return str(self.slug)
