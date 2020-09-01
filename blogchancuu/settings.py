5adba6f5013be2d59448b8b8af33c898d3d19fa8
tuan anh <canhazn@gmail.com>
2020-09-01 07:47:41 +0700

    Merge relase

--- blogchancuu/settings.py
+++ blogchancuu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vy)j7o@4#8v11_56c00hf%&99nxlkb5&fo(1o7!g!cx2t_nug5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
